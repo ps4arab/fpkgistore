afc5aec296aee6182108d5b7384ce1774911b24c
ps4arab <162325520+ps4arab@users.noreply.github.com>
2025-02-24 03:57:21 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,8 @@
-function fetchData() {
-  fetch('countries.json')
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data)
-      let output = '<h2">Countries</h2>'
-      data.forEach(function (item) {
-        output += `
-        <ul>
-          <li>Country: ${item.Country}</li>
-          <li>CODE: ${item.description}</li>
-          <li>CODE: ${item.link_text}</li>
-        </ul>
-      `
-      })
-      document.getElementById('country').innerHTML = output
-    })
-    .catch((error) => {
-      console.log(`Error Fetching data : ${error}`)
-      document.getElementById('country').innerHTML = 'Error Loading Data'
-    })
-}
-fetchData()
+$.getJSON('example.json', function(data) {
+      var output = '';
+      $.each(data.name, function(key, value) {
+            output += '<a href="emailto:' + value.email + '">'  + value.name + '</a>';
+    });
+
+    $('#user').html(output);
+});
